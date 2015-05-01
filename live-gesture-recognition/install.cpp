403c1052eadade6bb1a23f9df961f544c4e6290d
Mukul Shukla <mukul.shukla1992@gmail.com>
2015-05-01 12:20:02 +0530

    Comments added

--- live-gesture-recognition/install.cpp
+++ live-gesture-recognition/install.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main(){
 
-	system("g++ `pkg-config opencv --cflags` initialize.cpp -o initialize `pkg-config opencv --libs` -lX11 -lXtst");
-	system("g++ `pkg-config opencv --cflags` addgesture.cpp -o addgesture `pkg-config opencv --libs` -lX11 -lXtst");
-	system("g++ `pkg-config opencv --cflags` checkgesture.cpp -o checkgesture `pkg-config opencv --libs` -lX11 -lXtst");
-	system("g++ `pkg-config opencv --cflags` delgesture.cpp -o delgesture `pkg-config opencv --libs` -lX11 -lXtst");
-	system("g++ `pkg-config opencv --cflags` main.cpp -o main `pkg-config opencv --libs` -lX11 -lXtst");
-	system("g++ `pkg-config opencv --cflags` gesture.cpp -o gesture `pkg-config opencv --libs` -lX11 -lXtst");
+	system("g++ `pkg-config opencv --cflags` initialize.cpp -g -o initialize `pkg-config opencv --libs` -lX11 -lXtst");
+	system("g++ `pkg-config opencv --cflags` addgesture.cpp -g -o addgesture `pkg-config opencv --libs` -lX11 -lXtst");
+	system("g++ `pkg-config opencv --cflags` checkgesture.cpp -g -o checkgesture `pkg-config opencv --libs` -lX11 -lXtst");
+	system("g++ `pkg-config opencv --cflags` delgesture.cpp -g -o delgesture `pkg-config opencv --libs` -lX11 -lXtst");
+	system("g++ `pkg-config opencv --cflags` main.cpp -g -o main `pkg-config opencv --libs` -lX11 -lXtst");
+	system("g++ `pkg-config opencv --cflags` gesture.cpp -g -o gesture `pkg-config opencv --libs` -lX11 -lXtst");
 
 return 0;
 }
